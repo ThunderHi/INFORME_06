5355542f192aba11660583658b8287eba67f6d57
ThunderHi <161666073+ThunderHi@users.noreply.github.com>
2024-05-07 00:52:11 -0500

    Comentado de experiencia 04

--- Act04.cpp
+++ Act04.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   //inclusion de librería iostream
+using namespace std;  //uso de espacio standar
 
-class Vector {
+class Vector {  //creacion de clase Vector
 public:
-  float x;
+  float x;    //creacion de atributos
   float y;
 
   // Constructor
   Vector(float x, float y) : x(x), y(y) {}
 
   // Operador suma (+) sobrecargado
-  Vector operator+(const Vector& otroVector) {
+  Vector operator+(const Vector& otroVector) {  //se envia objeto como parametro
     return Vector(x + otroVector.x, y + otroVector.y);
   }
 
   // Operador resta (-) sobrecargado
-  Vector operator-(const Vector& otroVector) {
+  Vector operator-(const Vector& otroVector) {  //se envia objeto como parametro
     return Vector(x - otroVector.x, y - otroVector.y);
   }
 
@@ -31,10 +31,12 @@ int main() {
   Vector vectorB(4.5, 5.2);
 
   // Suma de vectores
+  //se crea otro objeto y se asigna la suma
   Vector vectorC = vectorA + vectorB;
   vectorC.imprimir(); // Resultado: Vector: (6, 8)
 
   // Resta de vectores
+  //se crea otro objeto y se asigna la resta
   Vector vectorD = vectorA - vectorB;
   vectorD.imprimir(); // Resultado: Vector: (-2, -2)
 
