c25480e53f32791c31dffed2d1bdd028a7eb36f9
ThunderHi <161666073+ThunderHi@users.noreply.github.com>
2024-05-06 22:17:39 -0500

    Realización de la parte 1. Creación de la clase Alumno y sus respectivos atributos

---
+++ Eje1.py
@@ -0,0 +1,21 @@
+#Creación de atributos de la clase Alumno
+class Alumno:
+    #Se indican atributos privados usando doble subguion
+    __nombre = 'Desconocido'       #Nombre del alumno
+    __edad = 16                 #Edad del alumno
+    __promedio = 0.0            #Promedio del alumno
+    
+    #Creacion de getters y setters
+    def getNombre(self):        #Self es un parámetro implicito
+        return self.__nombre   #Se usa self para referirse a si mismo.
+    def getEdad(self):
+        return self.__edad
+    def getPromedio(self):
+        return self.__promedio
+    def setNombre(self, n):  #Se envian 02 parametros (implicto/explicito)
+        self.__nombre = n
+    def setEdad(self, n):
+        self.__edad = n
+    def setPromedio(self, n):
+        self.__promedio = n
+        
